8b56bc10c8dd340ea9c4713686242a17b9b67a1b
drummingduck <eric.drummingducktape@gmail.com>
2016-04-12 09:56:19 -0600

    Update Sudoku.py
    Can solve simple boards

--- Sudoku.py
+++ Sudoku.py
@@ -1,3 +1,96 @@
 #This is a sudoku solver. Imports a sudoku text file and prints the solution.
 
 #making edits
+
+class Sudoku(object):
+
+	def __init__(self, puzzle):
+		self.puzzle = puzzle
+		self.puzzle_array = [[0 for j in range(9)] for i in range(9)]
+		self.possibles_list = [[[1,2,3,4,5,6,7,8,9] for j in range(9)] for i in range(9)]
+		self.count = 0
+
+	def print_puzzle(self):
+		# for line in self.puzzle:
+		i = 0
+		while i <= 8:
+			print self.puzzle[i*9:9+i*9]
+			i = i + 1
+
+	def chop_puzzle(self):
+		# for line in self.puzzle:
+		for h in range(9):
+			for j in range(9):
+				a = self.puzzle[(h*9) + j]
+				if a == '.':
+					None
+				else:
+					self.puzzle_array[h][j] = int(a)
+		print self.puzzle_array
+
+	def print_board(self):
+		# for h in range(9):
+		print "board"
+		for row in self.puzzle_array:
+			print row
+
+	def possible_moves(self, x, y):
+		a = self.puzzle_array[x][y]
+		b = self.possibles_list[x][y]
+		modx = (x//3)*3
+		mody = (y//3)*3
+		if a == 0:
+			for i in range(9):
+				if self.puzzle_array[x][i] in b:
+					b.remove(self.puzzle_array[x][i])
+			for i in range(9):
+				if self.puzzle_array[i][y] in b:
+					b.remove(self.puzzle_array[i][y])
+			for i in range(modx , modx + 3 ):
+				for j in range (mody , mody + 3 ):
+					if self.puzzle_array[i][j] in b:
+						b.remove(self.puzzle_array[i][j])
+			self.possibles_list[x][y] = b
+		else:
+			self.possibles_list[x][y] = [a]
+		#print self.possibles_list[x][y]
+
+	def possible_moves_board(self):
+		self.count = 0
+		for i in range(9):
+			for j in range (9):
+				Sudoku.possible_moves(self,i,j)
+				if len(self.possibles_list[i][j]) == 1:
+					self.puzzle_array[i][j] = self.possibles_list[i][j][0]
+					Sudoku.print_board(self)
+					self.count = self.count + 1
+
+
+	def solve_sudoku(self):
+		while self.count != 81:
+			Sudoku.possible_moves_board(self)
+		Sudoku.print_board(self)
+
+
+
+
+
+
+
+
+
+
+
+board = ["game..", "game2..."]
+board = ".9..8..2.71.2....8.685........4.2.15.5.798.349..1..7.6.7.3.4.62.26..135...39..14."
+sudoku1 = Sudoku(board)
+
+sudoku1.print_puzzle()
+sudoku1.chop_puzzle()
+sudoku1.print_board()
+sudoku1.possible_moves(0,1)
+sudoku1.possible_moves_board()
+sudoku1.solve_sudoku()
+
+
+

4f8c5b518f6105a355a513e8dfaaf876c923ba82
Ben Jacobson <ben.j.jacobson@gmail.com>
2016-04-13 09:01:07 -0600

    Update Sudoku.py
    added some TODOs

--- Sudoku.py
+++ Sudoku.py
@@ -9,6 +9,7 @@ class Sudoku(object):
 		self.puzzle_array = [[0 for j in range(9)] for i in range(9)]
 		self.possibles_list = [[[1,2,3,4,5,6,7,8,9] for j in range(9)] for i in range(9)]
 		self.count = 0
+		# TODO: self.init_board()
 
 	def print_puzzle(self):
 		# for line in self.puzzle:
@@ -17,6 +18,8 @@ class Sudoku(object):
 			print self.puzzle[i*9:9+i*9]
 			i = i + 1
 
+	# TODO: maybe rename this to init_board or something
+	# and call it in the __init__
 	def chop_puzzle(self):
 		# for line in self.puzzle:
 		for h in range(9):
@@ -34,6 +37,7 @@ class Sudoku(object):
 		for row in self.puzzle_array:
 			print row
 
+	# TODO: instead of setting a possibles_list, this function should just return the list
 	def possible_moves(self, x, y):
 		a = self.puzzle_array[x][y]
 		b = self.possibles_list[x][y]
@@ -55,19 +59,32 @@ class Sudoku(object):
 			self.possibles_list[x][y] = [a]
 		#print self.possibles_list[x][y]
 
+	#
+	# TODO: implement this, using the logic in your email. You should be able to use possible_moves
+	#
+	def inferred_moves(self, i, j):
+		print "implement inferred_moves"
+		
 	def possible_moves_board(self):
 		self.count = 0
 		for i in range(9):
 			for j in range (9):
+				# you should call this like: self.possible_moves(i, j)
 				Sudoku.possible_moves(self,i,j)
+				# moves = self.possible_moves(i, j)
+				# if len(moves) == 1:
 				if len(self.possibles_list[i][j]) == 1:
 					self.puzzle_array[i][j] = self.possibles_list[i][j][0]
+					# self.print_board()
 					Sudoku.print_board(self)
 					self.count = self.count + 1
-
-
+	
 	def solve_sudoku(self):
+		# TODO: instead of self.count != 81
+		# maybe have a method called completed() which returns true/false
+		# while not self.completed():
 		while self.count != 81:
+			# self.find_and_fill_next_possible_move()
 			Sudoku.possible_moves_board(self)
 		Sudoku.print_board(self)
 

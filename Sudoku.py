9a70419e5b8aaa01831c4ba62b91f2e7868846e2
drummingduck <eric.drummingducktape@gmail.com>
2016-04-16 14:53:37 -0600

    Update Sudoku.py
    can solve 50 puzzles in sudokupuzzles.txt

--- Sudoku.py
+++ Sudoku.py
@@ -10,13 +10,8 @@ class Sudoku(object):
 		self.possibles_list = [[[1,2,3,4,5,6,7,8,9] for j in range(9)] for i in range(9)]
 		self.count = 0
 		self.runs = 0
+		self.breaks = 0
 
-	def print_puzzle(self):
-		# for line in self.puzzle:
-		i = 0
-		while i <= 8:
-			print self.puzzle[i*9:9+i*9]
-			i = i + 1
 
 	def chop_puzzle(self):
 		# for line in self.puzzle:
@@ -27,7 +22,6 @@ class Sudoku(object):
 					None
 				else:
 					self.puzzle_array[h][j] = int(a)
-		print self.puzzle_array
 
 	def print_board(self):
 		# for h in range(9):
@@ -59,14 +53,14 @@ class Sudoku(object):
 			#Check column, row, box for numbers already known
 			#and remove from possiilities list for each cell.
 			#----------------
-			#column
+			#row
 			#----------------
 			for i in range(9):
 				if i != y:
 					if self.puzzle_array[x][i] in b:
 						b.remove(self.puzzle_array[x][i])
 			#----------------
-			#row
+			#column
 			#----------------
 			for i in range(9):
 				if i != x:
@@ -80,9 +74,12 @@ class Sudoku(object):
 					if i != x and j != y:
 						if self.puzzle_array[i][j] in b:
 							b.remove(self.puzzle_array[i][j])
-			self.possibles_list[x][y] = b
 		else:
-			self.possibles_list[x][y] = [a]
+			b = [a]
+		self.possibles_list[x][y] = b
+		if len (b) == 1:
+			self.puzzle_array[x][y] = b[0]
+			self.count = self.count + 1
 
 	def inferred_moves(self, x, y):
 		a = self.puzzle_array[x][y]
@@ -94,7 +91,7 @@ class Sudoku(object):
 			#for each cell checks possibilities of each cell in column, row, box
 			#if possibility is not in any other cell it sets the cell to that possibility
 			#----------------
-			#column
+			#row
 			#----------------
 			for possibility in b:
 				possible_counter = 0
@@ -104,7 +101,7 @@ class Sudoku(object):
 				if possible_counter == 8:
 					b = [possibility]
 			#----------------
-			#row
+			#column
 			#----------------
 			for possibility in b:
 				possible_counter = 0
@@ -124,6 +121,8 @@ class Sudoku(object):
 							possible_counter = possible_counter + 1
 				if possible_counter == 8:
 					b = [possibility]
+		if len (b) == 1:
+			self.puzzle_array[x][y] = b[0]
 			self.possibles_list[x][y] = b
 
 	def rule_4(self, x, y):
@@ -133,7 +132,7 @@ class Sudoku(object):
 		mody = (y//3)*3
 		if a == 0:
 			for possibility in b:
-				#check row
+				#check column
 				tick = 0
 				for i in range(modx , modx + 3):
 					if possibility in self.possibles_list[i][y]:
@@ -143,14 +142,15 @@ class Sudoku(object):
 					for j in range(mody , mody + 3):
 						if possibility in self.possibles_list[i][j]:
 							possible_counter = possible_counter + 1
-				#remove possibilities from row outside box
+				#remove possibilities from column outside box
 				if possible_counter == tick:
 					for i in range(0, 9):
 						if i not in range(modx, modx + 3):
 							if possibility in self.possibles_list[i][y]:
 								c = self.possibles_list[i][y]
 								c.remove(possibility)
-				#check column
+								self.possibles_list[i][y] = c
+				#check cloumn
 				tick = 0
 				for i in range(mody , mody + 3):
 					if possibility in self.possibles_list[x][i]:
@@ -160,103 +160,103 @@ class Sudoku(object):
 					for j in range(mody , mody + 3):
 						if possibility in self.possibles_list[i][j]:
 							possible_counter = possible_counter + 1
-				#remove possibilities from row outside box
+				#remove possibilities from cloumn outside box
 				if possible_counter == tick:
 					for i in range(0, 9):
 						if i not in range(mody, mody + 3):
 							if possibility in self.possibles_list[x][i]:
 								c = self.possibles_list[x][i]
 								c.remove(possibility)
-		else:
-			None
-
-			#----------------
-	#def naked_pairs(self)
-	#def lines(self)
-			#http://byteauthor.com/2010/08/sudoku-solver-update/
-			#----------------
-			#----------------
-	# def guess(self):
-	# 	test_board = self.puzzle_array
-	# 	for i in range(9):
-	# 		for j in range (9):
-	# 			a = test_board[i][j]
-	# 			b = self.possibles_list[i][j]
-	# 			if a = 0:
-	# 				for possibility in b:
-
-
-
+								self.possibles_list[x][i] = c
 
 	def possible_moves_board(self):
 		self.count = 0
 		for i in range(9):
 			for j in range (9):
 				self.possible_moves(i,j)
-				self.inferred_moves(i,j)
-				self.rule_4(i,j)
-				moves = self.possibles_list[i][j]
-				if len(moves) == 1:
-					self.puzzle_array[i][j] = self.possibles_list[i][j][0]
-					self.count = self.count + 1
-		#if self.count >= 50:
-		#	self.guess()
 
+	def inferred_moves_board(self):
+		for i in range(9):
+			for j in range (9):
+				self.inferred_moves(i,j)
+		self.possible_moves_board()
+
+	def rule_4_board(self):
+		for i in range(9):
+			for j in range (9):
+				self.rule_4(i,j)
+		self.possible_moves_board()
+
+	def valid(self):
+		for i in range(9):
+			for j in range (9):
+				moves = self.possibles_list[i][j]
+				if len(moves) == 0:
+					return False
+		return True
+
+	def guess(self):
+		import copy
+		g = copy.deepcopy(self.puzzle_array)
+		h = copy.deepcopy(self.possibles_list)
+		for i in range(9):
+			e = copy.deepcopy(self.puzzle_array)
+			f = copy.deepcopy(self.possibles_list)
+			for j in range (9):
+				a = self.puzzle_array[i][j]
+				b = self.possibles_list[i][j]
+				if a == 0:
+					c = copy.deepcopy(self.puzzle_array)
+					d = copy.deepcopy(self.possibles_list)
+					for possibility in b:
+						self.possibles_list[i][j] = [possibility]
+						self.possible_moves_board()
+						self.inferred_moves_board()
+						self.rule_4_board()
+						self.inferred_moves_board()
+						if self.valid() == False:
+							self.puzzle_array = copy.deepcopy(e)
+							self.possibles_list = copy.deepcopy(f)
+						if self.valid() == True:
+							break
 
 	def solve_sudoku(self):
-		import time
-		start_time = time.time()
-		while self.runs != 10:
-			self.possible_moves_board()
-			self.runs = self.runs + 1
-			self.print_board()
-			print self.possibles_list[6][0]
-			print self.possibles_list[6][1]
-			print self.possibles_list[6][2]
-			print self.possibles_list[7][0]
-			print self.possibles_list[7][1]
-			print self.possibles_list[7][2]
-			print self.possibles_list[8][0]
-			print self.possibles_list[8][1]
-			print self.possibles_list[8][2]
-			print "bottom corner"
-			print self.possibles_list[6][6]
-			print self.possibles_list[6][7]
-			print self.possibles_list[6][8]
-			print self.possibles_list[7][6]
-			print self.possibles_list[7][7]
-			print self.possibles_list[7][8]
-			print self.possibles_list[8][6]
-			print self.possibles_list[8][7]
-			print self.possibles_list[8][8]
-			print self.count
+		self.chop_puzzle()
+		print "empty"
 		self.print_board()
-		print "number of runs through board:"
-		print self.runs
-		print("--- %s seconds ---" % (time.time() - start_time))
+		self.runs = 0
+		self.count = 0
+		while self.count != 81:
+			self.possible_moves_board()
+			self.inferred_moves_board()
+			self.rule_4_board()
+			self.inferred_moves_board()
+			self.guess()
+			self.runs = self.runs + 1
+			if self.runs > 3:
+				self.breaks = self.breaks + 1
+				print "------- break %d -------" % self.breaks
+				print self.puzzle
+				self.print_board()
+				quit()
+				break
+		print "filled"
+		self.print_board()
 
+file = open('sudokupuzzles.txt', 'r')
+import time
+start_time = time.time()
+for line in file:
+	board = str(line).strip("\n")
+	sudokupuzzles = Sudoku(board)
+	sudokupuzzles.solve_sudoku()
+print("--- %s seconds ---" % (time.time() - start_time))
 
-
-
-
-
-
-
-
-
-
-board = ["game..", "game2..."]
-board = ".9..8..2.71.2....8.685........4.2.15.5.798.349..1..7.6.7.3.4.62.26..135...39..14."
-board2 = ".94...13..............76..2.8..1.....32.........2...6.....5.4.......8..7..63.4..8"
-sudoku1 = Sudoku(board2)
-
-sudoku1.print_puzzle()
-sudoku1.chop_puzzle()
-sudoku1.print_board()
-sudoku1.possible_moves(0,1)
-sudoku1.possible_moves_board()
-sudoku1.solve_sudoku()
-
+# board = ["game..", "game2..."]
+# board = "300200000000107000706030500070009080900020004010800050009040301000702000000008006"
+# board2 = ".....7....9...1.......45..6....2.....36...41.5.....8.9........4....18....815...32"
+# sudoku1 = Sudoku(board2)
+# sudoku1.solve_sudoku()
 
 
 
